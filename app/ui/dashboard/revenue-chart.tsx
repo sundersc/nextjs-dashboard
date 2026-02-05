bf25fdf9cdaad9c8bb8d0e52f1e2ada8daec8dc6
Christopher Sundersingh <sundersc@amazon.com>
2026-02-05 12:52:03 -0800

    make component alone dynamic

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -2,6 +2,7 @@ import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue } from '@/app/lib/data';
+import { unstable_noStore as noStore } from 'next/cache';
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -10,6 +11,7 @@ import { fetchRevenue } from '@/app/lib/data';
 // https://airbnb.io/visx/
 
 export default async function RevenueChart() {
+  noStore();
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
   const revenue = await fetchRevenue();
